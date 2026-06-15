485c22c4474fe5408260045b3a8866731d2d6ff9
avinashJcodes <avinashjadhav80100@gmail.com>
2026-06-15 15:22:26 +0530

    Day 1 JavaScript Practice

---
+++ control flow/index.js
@@ -0,0 +1,16 @@
+
+//  switch case 
+let fruit = "apple"
+
+ switch(fruit){
+
+    case "banana":
+      console.log("Yellow")
+         break;
+     case "orange":
+       console.log("orange")
+          break;
+          case "apple":
+            console.log("red")
+            break ;
+ }       
