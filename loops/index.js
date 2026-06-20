a634831a87b9ad549bb1c364315a115ed4f9d061
avinashJcodes <avinashjadhav80100@gmail.com>
2026-06-20 09:01:11 +0530

    loops

--- loops/index.js
+++ loops/index.js
@@ -65,3 +65,93 @@ while (i <= 10) {
 
   i++;
 }
+
+
+//Q6. Find the sum of all numbers from 1 to 100 using a loop and print the final total at the end.
+
+let sum = 0;
+
+for (let i = 1; i <= 100; i++) {
+  sum = sum + i;
+}
+
+console.log(sum);
+
+
+
+// using for loop
+
+let sum = 0;
+let i = 1;
+
+while (i <= 100) {
+  sum = sum + i;
+
+  i++;
+}
+
+console.log(sum);
+
+
+// Question 7: Print all numbers between 1 to 50 that are divisible by 3
+
+     for(let i = 1 ; i<=50; i++){
+
+        if(i%3 === 0) 
+          console.log(i)
+
+     }
+
+
+     //## Question 8: Ask the user for a number and print whether each number from 1 to that number is even or odd.
+
+let val = prompt("give a number");
+
+for (let i = 1; i <= val; i++) {
+  if (i%2 === 0)
+    {
+           console.log(`${i} is even number `);
+    }
+  else {
+    console.log(`${i} is odd number `);
+  }
+}
+
+
+//"Count how many numbers between 1 to 100 are divisible by both 3 and 5."
+
+   for(let i = 1; i<=100; i++){
+           if(i%3===0 && i%5=== 0 ){
+
+            console.log(i)
+           }
+   }
+
+
+   //1 se 30 tak loop chalao:
+
+//3 se divide ho → "Fizz"
+//5 se divide ho → "Buzz"
+//3 aur 5 dono se divide ho → "FizzBuzz"
+//warna number print karo
+
+
+
+
+         for( let i = 1; i<=30; i++){
+
+             if (i%3 === 0 && i%5 === 0 ){
+
+                    console.log("FizzBuzz")
+             } else if(i%5 === 0){
+                   console.log("Buzz")
+             }   else if (i%3 === 0){
+
+                  console.log("Fizz")
+             } else{
+
+                   console.log(i)
+             }
+
+         }
+       
